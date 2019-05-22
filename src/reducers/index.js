5491ae2b32ae72f00e4db0dbf080bdbd276b8502
Michael Snowden <mtsnowden@gmail.com>
2019-05-21 20:42:30 -0500

    added reducer, store, provider

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function reducer(state, action) {
+    return state
+};
+
+export default reducer;
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createStore } from 'redux';
+import state from './state'
+import reducers from './reducers'
+
+const store = createStore(reducers, state) 
+
+export default store;
