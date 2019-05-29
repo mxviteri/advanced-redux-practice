4dc0d733a8a03fa22997fc6e71bc83017249a8c1
Michael Snowden <mtsnowden@gmail.com>
2019-05-29 17:15:40 -0500

    got it working

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import state from './state'
-import reducers from './reducers'
+import state from './state';
+import reducers from './reducers/index';
 
 const store = createStore(reducers, state) 
 
